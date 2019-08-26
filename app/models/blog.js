e7464d78f078e988b67389e82a39ab0505332201
Tee <teedia31@gmail.com>
2019-08-26 10:58:02 -0400

    Add Blog resource - add one to many relationship with Comment

---
+++ app/models/blog.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }],
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Example', blogSchema)
