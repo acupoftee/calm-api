c493fdeaa7ce48f6ea7ab9a5e017d9dc2e99df01
Tee <teedia31@gmail.com>
2019-08-26 11:01:32 -0400

    Update User model - Added a name property for profile display names - Established a one to many relationship with Blog

--- app/models/user.js
+++ app/models/user.js
@@ -6,6 +6,13 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true
   },
+  name: {
+    type: String
+  },
+  blogs: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Blog'
+  }],
   hashedPassword: {
     type: String,
     required: true
