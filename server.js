92e84e42750a16e4ec5e8b98f76c03a293d58045
Tee <teedia31@gmail.com>
2019-08-26 14:14:45 -0400

    Import blog and comment routes

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const cors = require('cors')
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
-
+const blogRoutes = require('./app/routes/blog_routes')
+const commentRoutes = require('./app/routes/comment_routes')
 // require error handling middleware
 const errorHandler = require('./lib/error_handler')
 
@@ -60,6 +61,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // register route files
 app.use(exampleRoutes)
 app.use(userRoutes)
+app.use(blogRoutes)
+app.use(commentRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
