7c7d5a015ff5ee57d5827ce0334558da4e12b39c
Tee <teedia31@gmail.com>
2019-08-26 10:49:50 -0400

    Add Comment Resource

---
+++ app/models/comment.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const commentSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Example', commentSchema)
